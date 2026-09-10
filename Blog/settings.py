ccd034da4be3709a899b63237f9f43de2296f167
Raj <r4121533@gmail.com>
2026-09-10 14:33:12 +0530

    postdb sift

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import  os
 from pathlib import Path
+import dj_database_url
 
 
 
@@ -76,14 +77,18 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"""
+DATABASES = DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
-
+"""
+DATABASES = {
+    'default':dj_database_url.config(
+        default = os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
